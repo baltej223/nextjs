41ea4d4b760182ed5940081aedef9b63e4c1a91a
baltej223 <baltej963@gmail.com>
2024-12-17 00:53:45 +0530

    Some changes done

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         {/* <Image src='vercel.svg' width={10} height={10} alt="Vercel Logo" /> */}
         <h1 className="text-4xl">Cooking Something</h1>
-        <p className="justify-items-center"><a style={{opacity:0.5}} href="https://github.com/baltej">@baltej</a></p>
+        <p className="justify-items-center"><a style={{opacity:0.5}} href="https://github.com/baltej223">@baltej</a></p>
         </main>
         {/* <Image src="/logo.svg" alt="Logo" width={100} height={100} /> */}
         {/* <div className="flex gap-4 items-center flex-col sm:flex-row"> */}
